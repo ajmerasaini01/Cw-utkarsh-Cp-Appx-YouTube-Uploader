0a09f1d14d8cb3a525dc062ddd54d7b47aab739a
ajmerasaini01 <ajmerasaini01@gmail.com>
2025-08-16 02:02:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) ACE 
+# (c) ACE
 
 import os
 
 class Config(object):
-    # get a token from @BotFather
-    pass
-    """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7363610742:AAHRdYOwGwg63pc3xnnFh1ZsEDDsOdhTpOM")
-    API_ID = int(os.environ["API_ID", 21179966]
-    API_HASH = os.environ["API_HASH", "d97919fb0a3c725e8bb2a25bbb37d57c"]
-    AUTH_USERS = "7326397503"""
- 
+    # Get a token from @BotFather
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", 26331872))
+    API_HASH = os.environ.get("API_HASH", "c93589620441707c37c5683a02eea54e")
+    AUTH_USERS = os.environ.get("AUTH_USERS", "8355707251")
+
